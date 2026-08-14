a875da1944c0d532aa56cdf53ce6b5dcb5e0f48f
LeoSuki819 <ryo.programation@gmail.com>
2026-08-14 08:17:13 -0300

    PHP initialized

---
+++ php/cadastroONG.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Dados do Cadastro da ONG</title>
+</head>
+<body>
+    <?php
+    
+    //inicializando variáveis com os valores recebidos
+    $nome = $_POST["nome"];
+    $cnpj = $_POST["cnpj"];
+    $email = $_POST["email"];
+    $telefone = $_POST["telefone"];
+    $senha = $_POST["senha"];
+    $confirmarsenha = $_POST["confirmar-senha"];
+    $termos = $_POST["termos"];
+
+    ?>
+</body>
+</html>
